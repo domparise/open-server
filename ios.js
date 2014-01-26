2745d6472cb64d73b7d8f55ff58b3994cae7ed65
Dom Parise <domparise@gmail.com>
2014-01-26 13:26:12 -0500

    functional web-service w/ socket.io

--- ios.js
+++ ios.js
@@ -4,27 +4,36 @@ var express = require('express'),
 	server = require('https').createServer(config.keys,app),
 	io = require('socket.io').listen(server);
 
-app.use(express.session());
-app.use(express.cookieParser({secret:'yolo'}));
 
 io.configure(function (){
-	io.set('authorization', function (handshakeData, callback) {
-		console.log(handshakeData);
-		callback(null, true); 
+	io.set('authorization', function (req, cb) {
+		req;
+		return cb(null, true); 
 	});
 });
 
 
 io.sockets.on('connection', function (socket) {
 
-    socket.emit('1join', function (data) {
-    	console.log('emit');
+    socket.on('open', function (req, cb) {
+    	// db action
+    	// push notify
+    	socket.broadcast.emit('1open',req.otb);
+    	return cb({}); // return 
     });
 
-    socket.on('auth', function (data, cb) {
-    	console.log(data);
-    	cb('sup');
+    socket.on('join', function (req, cb) {
+    	// db
+    	// push notify
+    	socket.broadcast.emit('2join',req.evt);
+    	return cb({}); // return event
     });
+
+    socket.on('newUser', function (req, cb) {
+    	// db
+    	return cb({}); // return user
+    })
+
 }); 
 
 server.listen(3000);
