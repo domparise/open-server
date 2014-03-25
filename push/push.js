fdee0a84c5038d72b62f4e3cefea914fa2941fce
domparise@gmail.com <domparise@gmail.com>
2014-03-25 09:15:14 -0400

    pushing to server

--- push/push.js
+++ push/push.js
@@ -4,12 +4,12 @@ var options = {'gateway':'gateway.sandbox.push.apple.com'};
 
 var apnConnection = new apn.Connection(options);
 
-var device = new apn.Device('ff8f9befdf05161087ef9b729b119b5d8e11432ff7cfbe86b78bb579025f650c');
+var device = new apn.Device('6de845f34b2dec914bd161feca1a9a3e6ea0b448818358cac1529313c7756619');
 
 var note = new apn.Notification();
 
 note.expiry = Math.floor(Date.now() / 1000) + 3600;
-note.badge = 3;
+note.badge = 1;
 note.sound = 'ping.aiff';
 note.alert = '\uD83D\uDCE7 \u2709 New message';
 note.payload = {'messageFrom':'Dom'};
